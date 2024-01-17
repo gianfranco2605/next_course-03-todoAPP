236314b8fc02282c8ab1742566127a9851766aaf
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2024-01-17 12:59:42 +0100

    todos components restAPI

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <>
-      <span className="text-5xl">Hola Mundo</span>
-    </>
-  );
-}
--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { Sidebar } from "./Sidebar";
-export { SidebarItem } from "./SidebarItem";
-export { TopMenu } from "./TopMenu";
-export { WidgetItem } from "./WidgetItem";
