6b855d0151d2cce6df4d334220f06da0023c9117
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2024-01-18 14:01:37 +0100

    server_components

--- src/todos/helpers/todos.ts
+++ src/todos/helpers/todos.ts
@@ -1,9 +1,19 @@
 import { Todo } from "@prisma/client";
 
+const sleep = (seconds: number = 0): Promise<boolean> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, seconds * 1000);
+  });
+};
+
 export const updateTodo = async (
   id: string,
   complete: boolean
 ): Promise<Todo> => {
+  await sleep(2);
+
   const body = { complete };
 
   const todo = await fetch(`/api/todos/${id}`, {
@@ -13,7 +23,32 @@ export const updateTodo = async (
       "Content-Type": "application/json",
     },
   }).then((res) => res.json());
-  console.log(todo);
 
   return todo;
+};
+
+export const createTodo = async (description: string): Promise<Todo> => {
+  const body = { description };
+
+  const todo = await fetch(`/api/todos`, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+
+  return todo;
+};
+
+export const deleteCompletedTodos = async (): Promise<boolean> => {
+  await fetch(`/api/todos`, {
+    method: "DELETE",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+
+  return true;
 };
